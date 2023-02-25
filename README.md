d480bf699db340cfd4ef4ae33768eca861a25faa
Mike Hoffm <mhoffm24@gmail.com>
2023-02-25 09:08:06 -0500

    Updated README. Reorganized JS

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 TOP Library Project
 
-This project is JS focused. The goal is to allow users to enter books to a personal library.
+This project is JS focused. Users can enter books to their personal library, remove them after displaying them, & toggle their read status.
 
-Book info is done via a form, which is then stored in an array as an object. These objects are then displayed as individual cards.
+Book info is entered via HTML form, which is then stored in an array as an object. These objects are then displayed as individual cards.
+
+What I took away from this project:
+
+- Additional pratice using HTML Forms
+- Additional practice using CSS Grid
+- JS Object Constructors, & Prototype Iheritance
+- Learned about event.preventDefault()
 
 Live Link: https://mikehoffm.github.io/library/
--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const addBook = document.getElementById('new-book');
 /* Form Variables */
 const formContainer = document.getElementById('form-container');
 const formSubmit = document.querySelector('.submit');
-let closeBtn = document.getElementById('close-form');
-
+const closeBtn = document.getElementById('close-form');
 
 // Remove form from DOM display
 function removeForm() {
   formContainer.style.display = 'none';
 }
 
-//Start page with form hidden
+// Start page with form hidden
 removeForm();
 
 // Object Constructor Function for Books
@@ -25,38 +24,36 @@ function Book(bookTitle, bookAuthor, bookPages, readStatus) {
   this.read = readStatus;
 }
 
-//TOGGLE FUNCTION
-Book.prototype.toggleStatus = function() {
-  if(this.read === true) {
+// TOGGLE FUNCTION
+Book.prototype.toggleStatus = function () {
+  if (this.read === true) {
     this.read = false;
   } else {
     this.read = true;
-  }}
+  }
+};
 
-
-// Collect user input from form 
+// Collect user input from form
 // Push through Object constructor & store in myLibrary
 function addBookToLibrary() {
-  let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
-  let pages = document.getElementById('pages').value;
-  let read = document.getElementById('read-status').checked;
- 
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read-status').checked;
+
   myLibrary.push(new Book(title, author, pages, read));
 }
 
-
 // Clears the card container of any individual cards
 function clearDisplay() {
-  while(cardContainer.firstChild) {
-   cardContainer.removeChild(cardContainer.firstChild);
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.firstChild);
   }
-   }
+}
 
 // Creates individual cards based on current content myLibrary array
 function displayBook() {
-  for(let i = 0; i < myLibrary.length; i++) {
-  
+  for (let i = 0; i < myLibrary.length; i++) {
     const newCard = document.createElement('div');
     newCard.classList.add('cards');
     cardContainer.appendChild(newCard);
@@ -64,69 +61,68 @@ function displayBook() {
     const cardTitle = document.createElement('p');
     cardTitle.classList.add('title-card');
     newCard.appendChild(cardTitle);
-    cardTitle.innerText = `Title: ${myLibrary[i].title}`
+    cardTitle.innerText = `Title: ${myLibrary[i].title}`;
 
     const cardAuthor = document.createElement('p');
     cardAuthor.classList.add('author-card');
     newCard.appendChild(cardAuthor);
-    cardAuthor.innerText = `Author: ${myLibrary[i].author}`
+    cardAuthor.innerText = `Author: ${myLibrary[i].author}`;
 
     const cardPages = document.createElement('p');
     cardPages.classList.add('pages-card');
     newCard.appendChild(cardPages);
-    cardPages.innerText = `${myLibrary[i].pages} pages`
+    cardPages.innerText = `${myLibrary[i].pages} pages`;
 
-    //Set data attribute on the newly created card, corresponding with it's array index
+    // Set data attribute on the newly created card, corresponding with it's array index
     newCard.dataset.index = i;
-    let bookIndex = newCard.dataset.index;
+    const bookIndex = newCard.dataset.index;
 
     const removeBtn = document.createElement('button');
-    removeBtn.setAttribute('class','remove-button');
+    removeBtn.setAttribute('class', 'remove-button');
     newCard.appendChild(removeBtn);
     removeBtn.innerText = 'Remove Book';
-    let btnIndex = bookIndex;
+    const btnIndex = bookIndex;
 
-    // Access the index of the button pressed, remove the corresponding book from library array & update display
-    removeBtn.addEventListener('click', () => { 
-      myLibrary.splice(btnIndex,1);
-      console.log(myLibrary);
+    // Access the index of the button pressed, remove the corresponding
+    // book from library array & update display
+    removeBtn.addEventListener('click', () => {
+      myLibrary.splice(btnIndex, 1);
       clearDisplay();
       displayBook();
-    })
+    });
 
-     //Create Ability to toggle read status 
+    // Create Ability to toggle read status
     const toggleRead = document.createElement('button');
-    toggleRead.setAttribute('class','toggle-read')
+    toggleRead.setAttribute('class', 'toggle-read');
     newCard.appendChild(toggleRead);
 
     if (myLibrary[i].read === true) {
       toggleRead.innerText = 'READ';
-    }
-     else { toggleRead.innerText= 'UNREAD'
+    } else {
+      toggleRead.innerText = 'UNREAD';
     }
 
-    toggleRead.addEventListener('click', ()=> {
-       myLibrary[i].toggleStatus();
-      if (myLibrary[i].read === false){
+    toggleRead.addEventListener('click', () => {
+      myLibrary[i].toggleStatus();
+      if (myLibrary[i].read === false) {
         toggleRead.innerText = 'UNREAD';
       } else {
         toggleRead.innerText = 'READ';
       }
-    })
-  
+    });
   }
-  } 
+}
 
- // Add New Book Button - Display a clear form each button press
-  addBook.addEventListener('click', () => {
+// Add New Book Button - Display a clear form each button press
+addBook.addEventListener('click', () => {
   document.getElementById('form').reset();
   formContainer.style.display = 'block';
 });
 
-//Close form without submitting button
-closeBtn.addEventListener('click', ()=> {
+// Close form without submitting button
+closeBtn.addEventListener('click', () => {
   removeForm();
-})
+});
 
 // When submit is pressed, add book values to library array
 // remove form from display
